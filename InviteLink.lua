8dd0f61b02ebafb3354ea431cda55fc418171c4b
TlDinhKhoi <luacrack4399@gmail.com>
2025-12-27 12:29:06 +0700

    Update InviteLink.lua

--- InviteLink.lua
+++ InviteLink.lua
@@ -1,4 +1,3 @@
 setclipboard("https://discord.com/invite/E2N7w35zkt")
 
 loadstring(game:HttpGet("https://github.com/TlDinhKhoi/Util/raw/refs/heads/main/NotifyBloxKid.luau"))()
-loadstring(game:HttpGet("https://github.com/TlDinhKhoi/Util/raw/refs/heads/main/ApiSex.lua"))()
