898ecd439363d7bfacca83ac69a02da86ac4a45d
TlDinhKhoi <luacrack4399@gmail.com>
2026-02-20 13:15:40 +0700

    Update InviteLink.lua

--- InviteLink.lua
+++ InviteLink.lua
@@ -1,5 +1,3 @@
-setclipboard("https://discord.com/invite/E2N7w35zkt")
-
 loadstring(game:HttpGet("https://github.com/TlDinhKhoi/Util/raw/refs/heads/main/NotifyBloxKid.luau"))()
 
 setclipboard("https://discord.gg/hATuHQaQRb")
